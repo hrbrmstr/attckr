eaa10ebe15a25b6a2601b1b721b3fe4444e96d5c
hrbrmstr <bob@rud.is>
2019-05-28 15:52:07 -0400

    R package repo initialization complete

---
+++ R/attckr-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/attckr>
+#' - BugReports: <https://gitlab.com/hrbrmstr/attckr/issues>
+#' 
+#' @md
+#' @name attckr
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
